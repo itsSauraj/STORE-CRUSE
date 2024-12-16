449ae8745c0b2ce744769a9ecf1065171731a74e
Saurabh Yadav <sauraj.contact@gmail.com>
2024-12-16 13:59:08 +0530

    Add redux-persist for state persistence; update store configuration and main entry point

--- src/main.jsx
+++ src/main.jsx
@@ -6,14 +6,17 @@ import App from './App.jsx'
 import { NotificationProvider } from './context/NotificationContext'
 
 import { Provider as ReduxProvider } from 'react-redux'
-import { store } from './redux/store.js'
+import { PersistGate } from 'redux-persist/integration/react'
+import { store, persistor } from './redux/store.js'
 
 createRoot(document.getElementById('root')).render(
 	<StrictMode>
 		<ReduxProvider store={store}>
-			<NotificationProvider>
-				<App />
-			</NotificationProvider>
+			<PersistGate loading={null} persistor={persistor}>
+				<NotificationProvider>
+					<App />
+				</NotificationProvider>
+			</PersistGate>
 		</ReduxProvider>
 	</StrictMode>,
 )
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 import { rootReducer } from "./rootReducer";
 import logger from "redux-logger";
 
+const persistConfig = {
+	key: 'root',
+	storage: storage,
+	blacklist: ['user'],
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-	reducer: rootReducer,
+	reducer: persistedReducer,
 	middleware: (getDefaultMiddleware) => {
 		return getDefaultMiddleware(
 			{
@@ -14,4 +24,5 @@ export const store = configureStore({
 	},
 });
 
+export const persistor = persistStore(store);
 
